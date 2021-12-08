32b51c61ebd2ae516d6bf8feea8f6ffd1a7d87e2
rogerio-app <rogerio@rogerio.app>
2021-12-08 15:05:21 -0300

    v1

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM alpine:3.13 AS baseimage
+
+RUN apk --no-cache add ca-certificates musl-dev wget bash curl
+RUN apk add ca-certificates && update-ca-certificates
+RUN apk add --update nodejs npm
+RUN apk add --update tzdata
+RUN mkdir /web
+COPY . /web
+WORKDIR /web
+RUN npm install
+
+EXPOSE 80
+
+CMD [ "node","server.js" ]
---
+++ server.js
@@ -0,0 +1,32 @@
+//Instrumentando o Application Insights
+let appInsights = require('applicationinsights');
+//f6fe34c2-6c8f-4d3a-bcce-73ae4bffd2e3
+//const instrumentationKey = process.env["APPINSIGHTS_INSTRUMENTATIONKEY"]
+const instrumentationKey = "4b36a6dc-9dc4-40bc-9b4c-8c056bde6d91"
+appInsights.setup(instrumentationKey).start()
+const telemetry = appInsights.defaultClient;
+
+
+const express = require('express');
+const app = express();
+const cors = require('cors')
+
+const bodyParser = require('body-parser')
+app.use(cors())
+app.use( bodyParser.json() ); 
+app.use(bodyParser.urlencoded({ 
+    extended: true
+}));
+
+
+app.use(express.static(__dirname+"/"));
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+
+//process.env.PORT
+const listener = app.listen(80, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
+  });
