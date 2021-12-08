04a6f315ff9ee06b29aa5084ca0572d8a9563a50
rogerio-app <rogerio@rogerio.app>
2021-12-08 15:42:53 -0300

    v5

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-//Instrumentando o Application Insights
-let appInsights = require('applicationinsights');
-//f6fe34c2-6c8f-4d3a-bcce-73ae4bffd2e3
 //const instrumentationKey = process.env["APPINSIGHTS_INSTRUMENTATIONKEY"]
 const instrumentationKey = "4b36a6dc-9dc4-40bc-9b4c-8c056bde6d91"
-appInsights.setup(instrumentationKey).start()
-const telemetry = appInsights.defaultClient;
+const appInsights = new ApplicationInsights({ config: {
+  instrumentationKey: instrumentationKey,
+  distributedTracingMode: DistributedTracingModes.W3C
+  /* ...Other Configuration Options... */
+} });
+appInsights.loadAppInsights();
+appInsights.trackPageView();
 
 
 const express = require('express');
@@ -24,6 +26,7 @@ app.use(express.static(__dirname+"/"));
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
+
 
 //process.env.PORT
 const listener = app.listen(8080, function() {
